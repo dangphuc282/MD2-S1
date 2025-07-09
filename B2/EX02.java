effb51af851365a0b359df4089a66740c76cca2b
dangphuc282 <dangphuc28022002@gmail.com>
2025-07-09 11:33:08 +0900

    Add files via upload

---
+++ B2/EX02.java
@@ -0,0 +1,10 @@
+public class EX02 {
+    public static void main(String[]args){
+        String name = "Dang Trong Phuc";
+        int age = 23;
+        String adress = "Thanh Hoa";
+
+        System.out.printf("Toi ten la "+ name + ", toi " + age+" tuoi. Hien dang hoc tap va sinh song tai nhat ban," + " que toi o "+ adress);
+    }
+}
+
